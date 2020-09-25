10e5cbd48c346c74e92d40fbf370f8c8928cb236
illiteratecoder <michael.gezahegn997@gmail.com>
2020-09-25 16:46:57 +0300

    sign_in page minor change.

--- lib/pages/authentication/login.dart
+++ lib/pages/authentication/login.dart
@@ -1,10 +1,12 @@
+import 'package:berbera_2/pages/authentication/register.dart';
+import 'package:berbera_2/pages/supplierMain.dart';
 import 'package:berbera_2/services/auth.dart';
+import 'package:berbera_2/widget/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:berbera_2/widget/provider.dart';
-import 'package:berbera_2/pages/supplierMain.dart';
+import 'package:page_transition/page_transition.dart';
 
-class  LoginPage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -19,7 +21,12 @@ class _LoginPageState extends State<LoginPage> {
   String _error = '';
 
   void _next_page() {
-    Navigator.of(context).pushReplacementNamed('/register');
+    Navigator.push(
+        context,
+        PageTransition(
+            type: PageTransitionType.downToUp,
+            duration: Duration(milliseconds: 500),
+            child: Register()));
   }
 
   @override
@@ -49,158 +56,158 @@ class _LoginPageState extends State<LoginPage> {
       ],
     ),
 */
-      body: Builder(
-        builder: (context) =>
-            Container(
-              padding: const EdgeInsets.symmetric(vertical:20.0,horizontal: 50.0),
-              child: Form(
-                key: _formKey,
-                onChanged: _onFormChange,
-                child: Center(
-                  child: ListView(
-                      shrinkWrap: true,
-                      children: <Widget>[
+          body: Builder(
+            builder: (context) =>
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical:20.0,horizontal: 50.0),
+                  child: Form(
+                    key: _formKey,
+                    onChanged: _onFormChange,
+                    child: Center(
+                      child: ListView(
+                          shrinkWrap: true,
+                          children: <Widget>[
 
-                        Image(image: AssetImage('images/logo.png'),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Text(
-                            'Berbera Market',
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 30,
-                              letterSpacing: -0.384,
-                              fontWeight: FontWeight.w700,
-                              height: 1.5,
+                            Image(image: AssetImage('images/logo.png'),
                             ),
-                            textAlign: TextAlign.center,
-                          ),),
-
-
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            //onSaved: (String val) => this._email = val,
-                            onChanged: (String val) => this._email = val,
-                            decoration: InputDecoration(
-
-                              hintText: 'Email',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12.00),
-                                gapPadding: 5,
-                              ),
-
-                            ),
-
-                          autovalidate: _formChanged,
-                          validator: (String val) {
-                            if (val.isEmpty)
-                              return "Enter an email";
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: TextFormField(
-                          focusNode: focusNode,
-                          obscureText: true,
-                          onChanged: (String val) => this._password = val,
-                          //onSaved: (String val) => this._store.storeName = val,
-                          decoration: InputDecoration(
-
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12.00),
-                              gapPadding: 5,),
-
-                            hintText: "Password",
-                          ),
-                          validator: (String val) {
-                            if (val.length < 8) {
-                              return "Enter a password 8+ char long";
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            margin: EdgeInsets.only(top: 10),
-                            height: 50,
-                            child: RaisedButton(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(80.0),
-                              ),
-                              color: Color(0xffe9902e),
-                              child: Text("Sign in",
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Text(
+                                'Berbera Market',
                                 style: TextStyle(
-                                  fontSize: 25,
-                                  color: Colors.white,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 30,
+                                  letterSpacing: -0.384,
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.5,
                                 ),
+                                textAlign: TextAlign.center,
+                              ),),
+
+
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                //onSaved: (String val) => this._email = val,
+                                onChanged: (String val) => this._email = val,
+                                decoration: InputDecoration(
+
+                                  hintText: 'Email',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.00),
+                                    gapPadding: 5,
+                                  ),
+
+                                ),
+
+                                autovalidate: _formChanged,
+                                validator: (String val) {
+                                  if (val.isEmpty)
+                                    return "Enter an email";
+                                  return null;
+                                },
                               ),
-                              onPressed: _formChanged
-                                  ? () async {
-                                if (_formKey.currentState.validate()) {
-                                  dynamic result = await auth
-                                      .signInWithEmailAndPassword(
-                                      _email, _password);
-                                  if (result == null) {
-                                    setState(() =>
-                                    _error =
-                                    'Incorrect email address or password!');
-                                  } else {
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: TextFormField(
+                                focusNode: focusNode,
+                                obscureText: true,
+                                onChanged: (String val) => this._password = val,
+                                //onSaved: (String val) => this._store.storeName = val,
+                                decoration: InputDecoration(
+
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.00),
+                                    gapPadding: 5,),
+
+                                  hintText: "Password",
+                                ),
+                                validator: (String val) {
+                                  if (val.length < 8) {
+                                    return "Enter a password 8+ char long";
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  height: 50,
+                                  child: RaisedButton(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(80.0),
+                                    ),
+                                    color: Color(0xffe9902e),
+                                    child: Text("Sign in",
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    onPressed: _formChanged
+                                        ? () async {
+                                      if (_formKey.currentState.validate()) {
+                                        dynamic result = await auth
+                                            .signInWithEmailAndPassword(
+                                            _email, _password);
+                                        if (result == null) {
+                                          setState(() =>
+                                          _error =
+                                          'Incorrect email address or password!');
+                                        } else {
 //                                Navigator.of(context).pushNamed(
 //                                    '/supp');
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => SupplierMain(),
-                                        settings: RouteSettings(
-                                            arguments: ''
-                                        ),
-                                      ),
-                                    );
-                                  }
-                                } else {
-                                  FocusScope.of(context).requestFocus(
-                                      focusNode);
-                                }
-                              }
-                                  : null,
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => SupplierMain(),
+                                              settings: RouteSettings(
+                                                  arguments: ''
+                                              ),
+                                            ),
+                                          );
+                                        }
+                                      } else {
+                                        FocusScope.of(context).requestFocus(
+                                            focusNode);
+                                      }
+                                    }
+                                        : null,
+                                  ),
+                                )
                             ),
-                          )
+                            SizedBox(height: 10.0),
+                            Row(
+
+                              children: [Text("Don't have an account?"), FlatButton(
+                                onPressed: () {
+                                  _next_page();
+                                },
+                                child: Text('Sign UP', style: TextStyle(
+                                  color: Color(0xffe9902e),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),),)
+                              ],),
+
+                            Text(
+                              _error,
+                              style: TextStyle(color: Colors.red, fontSize: 14.0),
+                            )
+                          ]
+
+
                       ),
-                        SizedBox(height: 10.0),
-                        Row(
-
-                          children: [Text("Don't have an account?"), FlatButton(
-                            onPressed: () {
-                              _next_page();
-                            },
-                            child: Text('Sign UP', style: TextStyle(
-                              color: Color(0xffe9902e),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),),)
-                          ],),
-
-                        Text(
-                          _error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0),
-                        )
-                      ]
-
-
+                    ),
                   ),
-                ),
-              ),
 
-            ),
-      ),
-      ));
+                ),
+          ),
+        ));
   }
 
   void _onFormChange() {
