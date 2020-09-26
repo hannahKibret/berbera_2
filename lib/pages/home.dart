dc64a0919450546840d97c0ebd7696c3f6f131a6
illiteratecoder <michael.gezahegn997@gmail.com>
2020-09-27 01:32:45 +0300

    Home_page Dashboard UI change.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-
 
 class Home extends StatefulWidget {
   @override
@@ -14,7 +15,9 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
 
   var dateTextStyle =
-  TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold,);
+  TextStyle(
+    fontSize: 18, color: Color(0xFF004A82), fontWeight: FontWeight.bold,);
+
 
   int touchedIndex;
 
@@ -26,38 +29,41 @@ class HomeState extends State<Home> {
       body: Container(
         color: Color(0xffE5E5E5),
         child: StaggeredGridView.count(
+          padding: EdgeInsets.all(8),
           crossAxisCount: 4,
-          crossAxisSpacing: 12.0,
-          mainAxisSpacing: 12.0,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 8.0,
           children: <Widget>[
+
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(top: 0),
+              child: myTextItems("New Orders", "No New Order"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 0),
+              child: myStockTab(68),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: myTextItems("Total Sold Items", "1262"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
               child: myLineChart(),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: myPiChart(),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: myTextItems("Mktg. Spend", "48.6M"),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: myTextItems("Latest", "25.5M"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: myLineChart(),
-            ),
 
           ],
           staggeredTiles: [
-            StaggeredTile.extent(4, 350.0),
-            StaggeredTile.extent(4, 400.0),
-            StaggeredTile.extent(2, 120.0),
-            StaggeredTile.extent(2, 120.0),
-            StaggeredTile.extent(4, 250.0),
+
+            StaggeredTile.extent(2, 150.0),
+            StaggeredTile.extent(2, 312.0),
+            StaggeredTile.extent(2, 150.0),
+            StaggeredTile.extent(4, 450.0),
+
           ],
         ),
       ),
@@ -146,92 +152,106 @@ class HomeState extends State<Home> {
       color: Colors.white,
       elevation: 14.0,
       borderRadius: BorderRadius.circular(24),
-      shadowColor: Color(0x802196F3),
+      shadowColor: Color(0xFF707070),
       child: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: SizedBox(
-          height: 250,
-          child: LineChart(
-            LineChartData(
-              lineTouchData: LineTouchData(enabled: true,),
-              lineBarsData: [
-                LineChartBarData(
-                  spots: [
-                    FlSpot(0, 4),
-                    FlSpot(1, 3.5),
-                    FlSpot(2, 4.5),
-                    FlSpot(3, 1),
-                    FlSpot(4, 4),
-                    FlSpot(5, 6),
-                  ],
-                  isCurved: true,
-                  barWidth: 8,
-                  colors: [
-                    Colors.orange,
-                  ],
-                  belowBarData: BarAreaData(
-                    show: true,
-                    colors: [Colors.deepOrange, Colors.orange, Colors.white],
-
-                  ),
-                  /*   aboveBarData: BarAreaData(
-                          show: true,
-                          colors: [Colors.orange.withOpacity(0.6)],
-                          cutOffY: cutOffYValue,
-                          applyCutOffY: true,
-                        ), */
-                  dotData: FlDotData(
-                    show: false,
-                  ),
-                ),
-              ],
-              minY: 0,
-              titlesData: FlTitlesData(
-                bottomTitles: SideTitles(
-                    showTitles: true,
-                    reservedSize: 10,
-                    textStyle: dateTextStyle,
-                    getTitles: (value) {
-                      switch (value.toInt()) {
-                        case 0:
-                          return 'Jan';
-                        case 1:
-                          return 'Feb';
-                        case 2:
-                          return 'Mar';
-                        case 3:
-                          return 'Apr';
-                        case 4:
-                          return 'May';
-                        case 5:
-                          return 'Jun';
-                        default:
-                          return '';
-                      }
-                    }),
-                leftTitles: SideTitles(
-                  showTitles: true,
-                  textStyle: dateTextStyle,
-                  getTitles: (value) {
-                    return '${value + 1}';
-                  },
-                ),
-              ),
-              axisTitleData: FlAxisTitleData(
-                  leftTitle: AxisTitle(
-                      showTitle: true, titleText: 'Value', margin: 4),
-                  bottomTitle: AxisTitle(
-                      showTitle: true,
-                      margin: 4,
-                      titleText: '2019',
-                      textStyle: dateTextStyle,
-                      textAlign: TextAlign.center)),
-              gridData: FlGridData(
-                show: false,
-              ),
-              borderData: FlBorderData(show: false),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Monthly Sales", style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+                color: Color(0xFF004A82),
+              )),
             ),
-          ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 25),
+              child: LineChart(
+                LineChartData(
+                  lineTouchData: LineTouchData(enabled: true,),
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: [
+                        FlSpot(0, 1),
+                        FlSpot(1, 2.5),
+                        FlSpot(2, 2),
+                        FlSpot(3, 4),
+                        FlSpot(4, 3),
+                      ],
+                      isCurved: false,
+                      barWidth: 8,
+                      colors: [
+                        Colors.orange,
+                      ],
+                      belowBarData: BarAreaData(
+                        show: false,
+                        colors: [
+                          Colors.deepOrange,
+                          Colors.orange,
+                          Colors.white
+                        ],
+
+                      ),
+
+                      dotData: FlDotData(
+                        show: true,
+                      ),
+                    ),
+                  ],
+                  minY: 0,
+                  titlesData: FlTitlesData(
+                    bottomTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 12,
+                        textStyle: dateTextStyle,
+                        getTitles: (value) {
+                          switch (value.toInt()) {
+                            case 0:
+                              return 'Jan';
+                            case 1:
+                              return 'Feb';
+                            case 2:
+                              return 'Mar';
+                            case 3:
+                              return 'Apr';
+                            case 4:
+                              return 'May';
+                            default:
+                              return '';
+                          }
+                        }),
+                    leftTitles: SideTitles(
+                      reservedSize: 25,
+                      showTitles: true,
+                      textStyle: dateTextStyle,
+                      getTitles: (value) {
+                        return '${value + 1}K';
+                      },
+                    ),
+                  ),
+                  axisTitleData: FlAxisTitleData(
+                      leftTitle: AxisTitle(
+                          reservedSize: 12,
+                          showTitle: true,
+                          titleText: 'Value',
+                          textStyle: dateTextStyle,
+                          margin: 12),
+                      bottomTitle: AxisTitle(
+                          showTitle: true,
+                          margin: 4,
+                          titleText: '2019',
+                          textStyle: dateTextStyle,
+                          textAlign: TextAlign.center)),
+                  gridData: FlGridData(
+                    show: true,
+                  ),
+                  borderData: FlBorderData(show: false),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -242,39 +262,99 @@ class HomeState extends State<Home> {
     return Material(
       color: Colors.white,
       elevation: 14.0,
-      borderRadius: BorderRadius.circular(24.0),
-      shadowColor: Color(0x802196F3),
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(title, style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.blueAccent,
-                    ),),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(subtitle,),
-                  ),
-
-                ],
-              ),
-            ],
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Colors.black12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: Text(title, style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25.0,
+              color: Color(0xFF004A82),
+            ),),
           ),
+
+          Padding(
+            padding: EdgeInsets.only(top: 30.0),
+            child: Text(subtitle, style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF707070)
+            ),),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Material myStockTab(int val) {
+    var min_val = 0 + val;
+    var max_val = 100 - val;
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Colors.black12,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          // mainAxisAlignment:MainAxisAlignment.center,
+          children: <Widget>[
+
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Text("Stock", style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+                color: Color(0xFF004A82),
+              ),),
+            ),
+            Column(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: AnimatedCircularChart(
+                    duration: Duration(milliseconds: 1000),
+                    size: const Size(200.0, 200.0),
+                    initialChartData: [
+                      new CircularStackEntry(
+                        <CircularSegmentEntry>[
+                          new CircularSegmentEntry(
+                              max_val.toDouble(), Colors.white, rankKey: 'Q1'),
+                          new CircularSegmentEntry(
+                              min_val.toDouble(), Color(0xff40b24b),
+                              rankKey: 'Q4'),
+                        ],
+                        rankKey: 'Quarterly Profits',
+                      ),
+                    ],
+                    startAngle: -10,
+                    holeRadius: 20,
+                    chartType: CircularChartType.Radial,
+                  ),
+                ),
+                Text("$val%", style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                  color: Color(0xFF004A82),
+                ),
+                  textAlign: TextAlign.center,),
+              ],),
+/*
+
+*/
+
+
+          ],
         ),
       ),
     );
   }
+
 
   List<PieChartSectionData> showingSections() {
     return List.generate(4, (i) {
